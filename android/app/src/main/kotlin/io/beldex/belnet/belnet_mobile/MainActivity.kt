51809c7943cb0808bfecc57996c8a7630d74759e
gabsid <gabsid@beldex.io>
2022-08-09 18:43:55 +0530

    splash screen and background images added

--- android/app/src/main/kotlin/io/beldex/belnet/belnet_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/io/beldex/belnet/belnet_mobile/MainActivity.kt
@@ -1,6 +1,28 @@
 package io.beldex.belnet
-
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.android.FlutterActivity
-
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.SplashScreen
+import io.beldex.belnet.SplashView
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine) // add this line
+      }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Aligns the Flutter view vertically with the window.
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // Disable the Android splash screen fade out animation to avoid
+            // a flicker before the similar frame is drawn in Flutter.
+            splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+        }
+
+        super.onCreate(savedInstanceState)
+    }
+      override fun provideSplashScreen(): SplashScreen? = SplashView()
+     // override fun provideSplashScreen(): SplashScreen? = SplashView()
 }

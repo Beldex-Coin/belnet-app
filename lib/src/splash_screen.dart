93f3a36cc3ef346fab6e2a2039ecdace0f3166c5
Gabsid <gabsid@beldex.io>
2025-05-12 12:17:39 +0530

    Add :Splash screen

--- lib/src/splash_screen.dart
+++ lib/src/splash_screen.dart
@@ -1,6 +1,8 @@
+import 'package:belnet_mobile/bottom_nav_bar.dart';
 import 'package:belnet_mobile/main.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:video_player/video_player.dart';
 
 
 class SplashScreens extends StatefulWidget {
@@ -12,17 +14,39 @@ class SplashScreens extends StatefulWidget {
 
 class _SplashScreensState extends State<SplashScreens>
     with TickerProviderStateMixin {
- AnimationController? _controller;
+ //AnimationController? _controller;
+
+ late VideoPlayerController _videoController;
+
 
   @override
   void initState() {
-    _controller = AnimationController(vsync: this, duration: Duration(seconds:5));
+    // _controller = AnimationController(vsync: this, duration: Duration(seconds:5));
     super.initState();
+    _videoController = VideoPlayerController.asset('assets/images/video.webm')
+      ..initialize().then((_) {
+        setState(() {});
+        _videoController.play();
+      });
+
+    _videoController.addListener(() {
+      if (_videoController.value.position == _videoController.value.duration) {
+        _goToNextScreen();
+      }
+    });
+  }
+
+void _goToNextScreen() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => MainBottomNavbar() //BelnetHomePage()
+      ),
+    );
   }
 
 @override
   void dispose() {
-    _controller!.dispose();
+   // _controller!.dispose();
+    _videoController.dispose();
     super.dispose();
   }
 
@@ -31,17 +55,33 @@ class _SplashScreensState extends State<SplashScreens>
 
     return
       Scaffold(
-      backgroundColor: Color(0xff1C1C26),
-        body: Center(
-      child: Container(
-        child: Lottie.asset('assets/images/Splash_belnet_1 (1).json',    //belnet_splash.json
-            controller: _controller, onLoaded: (composition) {
-          _controller!
-            ..duration = composition.duration
-            ..forward().whenComplete(() => Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (context) => BelnetHomePage())));
-        }),
-      ),
-    ));
+      backgroundColor: Colors.black,// Color(0xff1C1C26),
+        body:
+         _videoController.value.isInitialized
+           ?
+           SizedBox.expand(
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: SizedBox(
+                  width: _videoController.value.size.width,
+                  height: _videoController.value.size.height,
+                  child: VideoPlayer(_videoController),
+                ),
+              ),
+            )
+          : 
+          Center(child: Image.asset('assets/images/belnet_ic.png',)),
+    //      Center(
+    //   child: Container(
+    //     child: Lottie.asset('assets/images/Splash_belnet_1 (1).json',    //belnet_splash.json
+    //         controller: _controller, onLoaded: (composition) {
+    //       _controller!
+    //         ..duration = composition.duration
+    //         ..forward().whenComplete(() => Navigator.pushReplacement(context,
+    //             MaterialPageRoute(builder: (context) => BelnetHomePage())));
+    //     }),
+    //   ),
+    // ),
+    );
   }
 }

b4955680f84fe1b98d3ebbd5a66b41aa696717d2
Gabsid <gabsid@beldex.io>
2025-07-07 14:52:39 +0530

    Splash screen animation updated

--- lib/src/splash_screen.dart
+++ lib/src/splash_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
+import 'package:belnet_lib/belnet_lib.dart';
 import 'package:belnet_mobile/bottom_nav_bar.dart';
 import 'package:belnet_mobile/main.dart';
+import 'package:belnet_mobile/src/providers/loader_provider.dart';
 import 'package:belnet_mobile/src/screens/onboarding_screen.dart';
+import 'package:belnet_mobile/src/vpn_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
@@ -16,43 +21,95 @@ class SplashScreens extends StatefulWidget {
 }
 
 class _SplashScreensState extends State<SplashScreens>
-    with TickerProviderStateMixin {
+    with WidgetsBindingObserver {
  //AnimationController? _controller;
 
  late VideoPlayerController _videoController;
-
+ Timer? timer;
 
   @override
   void initState() {
     // _controller = AnimationController(vsync: this, duration: Duration(seconds:5));
     super.initState();
-    _videoController = VideoPlayerController.asset('assets/images/dark_theme/splash_v2.webm')
-      ..initialize().then((_) {
-        setState(() {});
-        _videoController.play();
-      });
+   // _controller = AnimationController(vsync: this, duration: Duration(seconds:5));
 
-    _videoController.addListener((){
-      if (_videoController.value.position == _videoController.value.duration) {
+    WidgetsBinding.instance.addObserver(this);
+    // _videoController = VideoPlayerController.asset('assets/images/dark_theme/splash_video.mp4')
+    //   ..initialize().then((_) {
+    //     setState(() {});
+    //     _videoController.play();
+    //   });
+
+    // _videoController.addListener((){
+    //   if (_videoController.value.position == _videoController.value.duration) {
        
-        _goToNextScreen();
-      }
+    //     _goToNextScreen();
+    //   }
+    // });
+  timer = Timer(const Duration(seconds: 3), () {
+    _goToNextScreen();
+     // Navigator.pushReplacementNamed(context, '/home'); // or use Navigator.pushReplacement(...)
     });
+
   }
 
 void _goToNextScreen()async {
      SharedPreferences prefs = await SharedPreferences.getInstance();
   bool isFirstLaunch = prefs.getBool('isFirstLaunch') ?? true;
+   //getStatus(context);
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(builder: (_) => isFirstLaunch ? OnboardingScreen() : MainBottomNavbar() //BelnetHomePage()
       ),
     );
   }
 
+
+// @override
+//   void didChangeAppLifecycleState(AppLifecycleState state) {
+//   try{
+
+//      // Safely use the provider with `context.read<T>()`
+//     if (!mounted) return;
+
+//     final loaderVideoProvider = context.read<LoaderVideoProvider>();
+//    if (state == AppLifecycleState.paused || state == AppLifecycleState.detached) {
+
+//       //timer1?.cancel();
+//       //AwesomeNotifications().cancel(10); // Cancel the notification with id 10
+//     }
+//     if(state == AppLifecycleState.resumed){
+//       if(BelnetLib.isConnected){
+//         print('I AM INside ----->');
+//         loaderVideoProvider.setConnectionStatus(ConnectionStatus.CONNECTED);
+//         print('I AM INside 111----->${loaderVideoProvider.conStatus}');
+
+//       }
+//         print('I AM INside out 222----->${loaderVideoProvider.conStatus}');
+
+//     }
+//   }catch(e){
+// }
+   
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
 @override
   void dispose() {
-   // _controller!.dispose();
-    _videoController.dispose();
+    timer?.cancel();
+    //_controller!.dispose();
+   // _videoController.dispose();
+        WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
 
@@ -61,22 +118,29 @@ void _goToNextScreen()async {
 
     return
       Scaffold(
+        extendBody: true,
       backgroundColor: Colors.black,// Color(0xff1C1C26),
-        body:
-         _videoController.value.isInitialized
-           ?
-           SizedBox.expand(
-              child: FittedBox(
-                fit: BoxFit.cover,
-                child: SizedBox(
-                  width: _videoController.value.size.width,
-                  height: _videoController.value.size.height,
-                  child: VideoPlayer(_videoController),
-                ),
-              ),
-            )
-          : 
-          Center(child: Image.asset('assets/images/belnet_ic.png',)),
+         body:Center(
+          child: Image.asset('assets/images/dark_theme/Splash_1.gif',
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
+          ),
+         )
+        //  _videoController.value.isInitialized
+        //    ?
+        //    SizedBox.expand(
+        //       child: FittedBox(
+        //         fit: BoxFit.cover,
+        //         child: SizedBox(
+        //           width: _videoController.value.size.width,
+        //           height: _videoController.value.size.height,
+        //           child: VideoPlayer(_videoController),
+        //         ),
+        //       ),
+        //     )
+        //   : 
+        //   Center(child: Image.asset('assets/images/belnet_ic.png',)),
     //      Center(
     //   child: Container(
     //     child: Lottie.asset('assets/images/Splash_belnet_1 (1).json',    //belnet_splash.json
@@ -84,7 +148,7 @@ void _goToNextScreen()async {
     //       _controller!
     //         ..duration = composition.duration
     //         ..forward().whenComplete(() => Navigator.pushReplacement(context,
-    //             MaterialPageRoute(builder: (context) => BelnetHomePage())));
+    //             MaterialPageRoute(builder: (context) => MainBottomNavbar())));
     //     }),
     //   ),
     // ),
